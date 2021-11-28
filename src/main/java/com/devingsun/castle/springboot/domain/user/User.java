d443058574cc42859f4311afcfa016082a1c1cc2
devingsun <devingsun@gmail.com>
2021-11-28 17:21:42 +0900

    session을 annotation으로 처리

--- src/main/java/com/devingsun/castle/springboot/domain/user/User.java
+++ src/main/java/com/devingsun/castle/springboot/domain/user/User.java
@@ -16,12 +16,16 @@ public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false)
     private String email;
+
     @Column
     private String picture;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
